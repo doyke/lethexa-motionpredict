d4ff38e387c7ba6ef528df7ce7da4bd7b3288072
Tim Leerhoff <tleerhof@web.de>
2016-02-13 11:31:45 +0100

    Added motion test.

--- test/motionpredict.js
+++ test/motionpredict.js
@@ -5,6 +5,23 @@ var motionpredict = require((process.env.APP_DIR_FOR_CODE_COVERAGE || '../lib/')
 
 
 describe('quadEquation', function () {
+    describe('when position=[1,2,3], velocity=[1,1,1] and dt=1.0', function () {
+        it('should return a new position of [2,3,4]', function () {
+		var position = new vecmat.Vector3d(1,2,3);
+		var velocity = new vecmat.Vector3d(1,1,1);
+		var dt = 1.0;
+                var result = motionpredict.getPositionByVeloAndTime(position, velocity, dt);
+                var expected = new vecmat.Vector3d(2,3,4);
+
+                assert.deepEqual(expected, result);
+        });
+    })
+});
+
+
+
+
+describe('quadEquation', function () {
     describe('when p=1 and q=0.25', function () {
         it('should return one solution [-0.5]', function () {
 		var result = motionpredict.quadEquation(1.0, 0.25);
